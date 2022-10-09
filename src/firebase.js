ce797131ef11ac8d502715770924dc1ef43908a5
Clausdanut <claudiu.danut38@yahoo.com>
2022-10-10 01:12:21 +0300

    GATA PROIECTUL

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,9 @@
 import firebase from "firebase";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyA4fL5Sp-iTGy5Z7LU27zd6do7rd6OJf5g",
     authDomain: "magazinshoping-7a1a6.firebaseapp.com",
